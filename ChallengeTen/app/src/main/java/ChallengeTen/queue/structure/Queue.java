51f1a3dac84cd471d2d1a82c6ff0c82e3b231068
naeemalomari <naeemalomari96@gmail.com>
2021-11-11 19:38:15 +0200

    adding

--- ChallengeTen/app/src/main/java/ChallengeTen/queue/structure/Queue.java
+++ ChallengeTen/app/src/main/java/ChallengeTen/queue/structure/Queue.java
@@ -38,6 +38,23 @@ public class Queue {
     }
 
 
+
+    public  String duckDuckGoose(String[] arr,int k){
+        Queue queue=new Queue();
+        for (int i=0;i<arr.length;i++) {
+            queue.enqueue(arr[i]);
+        }
+        for (int i=0;i<arr.length-1;i++) {
+            for (int j = 1; j < k; j++) {
+                queue.enqueue(queue.dequeue());
+            }
+            queue.dequeue();
+        }
+        return queue.dequeue();
+    }
+
+
+
     public String peek() {
         return front.getData();
     }
